9cbeb16442e6931bbfcb14b068202619b32ec694
Iz056290 <Izayhacw@knights.ucf.edu>
2023-07-31 21:22:29 -0400

    Hw4
    compile the code

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -1,34 +1,120 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temporary arrays
+    int* L = (int*)malloc(n1 * sizeof(int));
+    int* R = (int*)malloc(n2 * sizeof(int));
+ 
+    // Copy data to temporary arrays
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temporary arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of L and R
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+ 
+    free(L);
+    free(R);
+}
+
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+ 
+        // Merge the sorted halves
+        merge(pData, l, m, r);
+    }
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	for (int i = 1; i < n; i++) {
+        int key = pData[i];
+        int j = i - 1;
+        while (j >= 0 && pData[j] > key) {
+            pData[j + 1] = pData[j];
+            j--;
+            extraMemoryAllocated += sizeof(int); // Count memory allocated for swapping
+        }
+        pData[j + 1] = key;
+    }
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+ for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (pData[j] > pData[j + 1]) {
+                int temp = pData[j];
+                pData[j] = pData[j + 1];
+                pData[j + 1] = temp;
+                extraMemoryAllocated += sizeof(int); // Count memory allocated for swapping
+            }
+        }
+    }	
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
+	 for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (pData[j] < pData[min_idx])
+                min_idx = j;
+        }
+        int temp = pData[i];
+        pData[i] = pData[min_idx];
+        pData[min_idx] = temp;
+        extraMemoryAllocated += sizeof(int); // Count memory allocated for swapping
+    }
 	
 }
 
